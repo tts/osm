0262e3feee9042f0e72ca2b08cc3bb7edadc56b2
Tuija <tuija.sonkkila@aalto.fi>
2021-12-05 15:58:34 +0200

    png rendering

--- atm.R
+++ atm.R
@@ -9,9 +9,10 @@ atms <- getbb("Helsinki Finland") %>%
   add_osm_feature(key = "amenity", value = "atm") %>%
   osmdata_sf()
 
-leaflet() %>% 
+m <- leaflet() %>% 
   addTiles() %>%
   addProviderTiles(providers$Stamen.TonerLines,
                    options = providerTileOptions(opacity = 0.15)) %>%
   addCircleMarkers(data = atms$osm_points, radius = 3, color = "black")
 
+mapview::mapshot(m, file = "atms.png")
