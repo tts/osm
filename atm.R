bcf81d035cb277ebcd6017af90ea4b31467bd0cd
Tuija <tuija.sonkkila@aalto.fi>
2021-12-05 15:52:07 +0200

    Initial commit

---
+++ atm.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(osmdata)
+library(leaflet)
+
+# ATMs
+
+atms <- getbb("Helsinki Finland") %>%
+  opq() %>%
+  add_osm_feature(key = "amenity", value = "atm") %>%
+  osmdata_sf()
+
+leaflet() %>% 
+  addTiles() %>%
+  addProviderTiles(providers$Stamen.TonerLines,
+                   options = providerTileOptions(opacity = 0.15)) %>%
+  addCircleMarkers(data = atms$osm_points, radius = 3, color = "black")
+
